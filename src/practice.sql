f1d822db2561341b376ee17b70fe947eb8750ba6
coderchomo <codermermarket@gmail.com>
2021-11-01 10:47:23 +0700

    cac thao tac thuc hanh SQL

--- src/practice.sql
+++ src/practice.sql
@@ -20,3 +20,31 @@ from student s
          join mark m on s.StudentID = m.StudentID
          join subject sub on m.SubID = sub.SubID
 where sub.SubName = 'CF';
+
+select StudentName
+from student
+where StudentName like 'h%';
+
+select ClassName
+from class
+where MONTH(StartDate) = 12;
+
+SELECT SubName
+FROM subject
+WHERE Credit <= 5
+  AND Credit >= 3;
+
+UPDATE class
+SET ClassID=5
+WHERE ClassID = 1;
+
+SELECT s.StudentName,s2.SubName,m.Mark
+FROM student s
+         join subject s2 on s.Status = s2.Status
+         join mark m on s.StudentID = m.StudentID
+order by Mark DESC ;
+SELECT s.StudentName,s2.SubName,m.Mark
+FROM student s
+         join subject s2 on s.Status = s2.Status
+         join mark m on s.StudentID = m.StudentID
+order by StudentName ASC ;
